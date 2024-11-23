02d453040971747402682e45ee99715ff7326dc9
Bruno Boettcher <nokumado@gmail.com>
2024-11-23 15:41:45 +0100

    added start of ls command

---
+++ bin/noh_shell.dart
@@ -0,0 +1,5 @@
+import 'package:noh_shell/noh_shell.dart' as noh_shell;
+
+void main(List<String> arguments) {
+  print('Hello world: ${noh_shell.calculate()}!');
+}
---
+++ lib/command.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+class Command {
+  final String name;
+  final String? executable;
+  final List<String> arguments;
+
+  Command({this.name = "command",this.executable, required this.arguments});
+
+  Future<ProcessResult> execute({
+    String? input,
+    IOSink? output,
+    IOSink? error,
+    bool debug = false,
+  }) async {
+    return Process.run(
+      executable??"",
+      arguments,
+      stdoutEncoding: null,
+      stderrEncoding: null,
+    );
+  }
+}
+
